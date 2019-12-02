65ee32f9c8fb39802406e50d07d6f8ca638139eb
Aocory <tanimu@tanimu.org>
2019-12-02 17:25:25 +0900

    update

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -8,7 +8,7 @@
     <PageNav v-bind="{ sidebarItems }" />
 
     <slot name="bottom" />
-    
+    <Ad/>
 <a href="https://stock.blogmura.com/ranking/in?p_cid=11016741" target="_blank" ><img src="https://b.blogmura.com/stock/88_31.gif" width="88" height="31" border="0" alt="にほんブログ村 株ブログへ" /></a><br /><a href="https://stock.blogmura.com/ranking/in?p_cid=11016741">にほんブログ村</a>
   </main>
 </template>
@@ -16,9 +16,9 @@
 <script>
 import PageEdit from '@theme/components/PageEdit.vue'
 import PageNav from '@theme/components/PageNav.vue'
-
+import Ad from '@theme/components/Ad.vue'
 export default {
-  components: { PageEdit, PageNav },
+  components: { PageEdit, PageNav,Ad },
   props: ['sidebarItems']
 }
 </script>
